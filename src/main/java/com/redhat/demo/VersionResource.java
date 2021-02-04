3283c85c1195591a25206ae83af67b7344e50c08
Gerald Nunn <gerald.b.nunn@gmail.com>
2021-02-03 16:49:18 -0800

    Add Version API

---
+++ src/main/java/com/redhat/demo/VersionResource.java
@@ -0,0 +1,34 @@
+package com.redhat.demo;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.json.Json;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+@Path("/api/version")
+@ApplicationScoped
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Tag(name = "Products", description = "An API to return the version of the API")
+public class VersionResource {
+
+    @GET
+    @Operation(summary = "API Version", description = "Get API version")
+    @Counted(name = "countGetVersion", description = "How many get version calls have been performed.", tags = {"type=counter","api=version", "method=getVersion"} )
+    @Timed(name = "perfGetVersion", description = "A measure of how long it takes to get version.", unit = MetricUnits.MILLISECONDS, tags = {"type=perf","api=version", "method=getVersion"})
+    public String get() {
+        return "1.0.0";
+    }
+
+}
